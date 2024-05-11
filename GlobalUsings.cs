beafc74a041be68d6135dad2d589b96092fb06d4
sunamo.cz_asus <radek.jancik@sunamo.cz>
2024-05-11 11:16:10 +0200

    Some projects are flat and buildable

--- GlobalUsings.cs
+++ GlobalUsings.cs
@@ -1,3 +1,9 @@
 global using SunamoExceptions.InSunamoIsDerivedFrom;
 global using System.Threading.Tasks;
 global using MsgReader.Outlook;
+global using System.Text;
+global using System.Collections.Generic;
+global using System.Collections;
+global using System.IO;
+global using System.Linq;
+
