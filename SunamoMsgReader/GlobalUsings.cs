492cda04a3741f7be260996189e25ff2fc7b0a54
Radek Jancik <radek.jancik@usys.cz>
2025-12-21 21:22:59 +0100

    25.12.21.1 feat: Added net10.0 to TargetFrameworks

--- SunamoMsgReader/GlobalUsings.cs
+++ SunamoMsgReader/GlobalUsings.cs
@@ -1,5 +1,3 @@
-// EN: Variable names have been checked and replaced with self-descriptive names
-// CZ: Názvy proměnných byly zkontrolovány a nahrazeny samopopisnými názvy
 global using System.Threading.Tasks;
 global using MsgReader.Outlook;
 global using System.Text;
