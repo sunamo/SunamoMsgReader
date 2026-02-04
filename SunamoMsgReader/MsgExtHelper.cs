014e2a90e85ab8753f0ad8220798ef7cab9ee280
Radek Jancik <radek.jancik@usys.cz>
2026-02-04 12:46:25 +0100

    26.2.4.1 feat: Enable SourceLink and debug symbols

--- SunamoMsgReader/MsgExtHelper.cs
+++ SunamoMsgReader/MsgExtHelper.cs
@@ -1,18 +1,33 @@
 namespace SunamoMsgReader;
 
+/// <summary>
+/// Helper class for working with MSG files (Outlook message format).
+/// Provides utilities to extract content from .msg files.
+/// </summary>
 public class MsgExtHelper
 {
-    public static MsgExtHelper ci;
+    /// <summary>
+    /// Singleton instance of MsgExtHelper.
+    /// </summary>
+    public static MsgExtHelper Instance = null!;
 
     private MsgExtHelper()
     {
     }
 
+    /// <summary>
+    /// Creates the singleton instance of MsgExtHelper.
+    /// </summary>
     public static void CreateInstance()
     {
-        ci = new MsgExtHelper();
+        Instance = new MsgExtHelper();
     }
 
+    /// <summary>
+    /// Writes the HTML body of a MSG file to an HTML file.
+    /// </summary>
+    /// <param name="path">Path to the input .msg file.</param>
+    /// <param name="htmlFile">Path to the output .html file.</param>
     public
 #if ASYNC
         async Task
--- SunamoMsgReader/_sunamo/SunamoExceptions/Exceptions.cs
+++ SunamoMsgReader/_sunamo/SunamoExceptions/Exceptions.cs
@@ -1,15 +1,13 @@
 namespace SunamoMsgReader._sunamo.SunamoExceptions;
 
-// EN: Variable names have been checked and replaced with self-descriptive names
-// CZ: Názvy proměnných byly zkontrolovány a nahrazeny samopopisnými názvy
 // © www.sunamo.cz. All Rights Reserved.
 internal sealed partial class Exceptions
 {
     #region Other
 
 #region IsNullOrWhitespace
-    readonly static StringBuilder sbAdditionalInfoInner = new();
-    readonly static StringBuilder sbAdditionalInfo = new();
+    readonly static StringBuilder additionalInfoInnerStringBuilder = new();
+    readonly static StringBuilder additionalInfoStringBuilder = new();
     #endregion
 #endregion
 }
